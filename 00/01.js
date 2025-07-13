86c42046afb0499486fc879c1504bdb703002133
mayank-vashishtt <mayankvashishtt@gmail.com>
2025-07-14 01:53:04 +0530

    hoisting in js

---
+++ 00/01.js
@@ -0,0 +1,51 @@
+// console.log("hello people")
+
+
+
+// will the code work with this ; any without this  -- true 
+
+// how many datatypes are there in js -- 8 
+// boolean, number, string, object, null, undefined, symbol, BigInt
+
+
+
+// console.log(a)
+// const a = 10 // undefined 
+// reason -- hoisting
+
+
+
+
+// diff between var let and const ? 
+
+
+
+
+
+
+// add();
+
+
+// const add = () => {
+//     console.log("hello people")
+ 
+// }
+
+// add();
+
+
+// add();
+// const add = ()  => {
+
+
+//     console.log(a);
+//     console.log(b);
+//     var a = 10;
+//     const b = 20;
+//     console.log("hello people")
+    
+// }
+// add();
+
+
+
