c553a2567ec570b8669fe4cc841aafeb21bf79ca
mayank-vashishtt <mayankvashishtt@gmail.com>
2025-07-16 01:42:09 +0530

    ecma, iife, complex function

--- 02/main.js
+++ 02/main.js
@@ -1,33 +1,4 @@
-
-
-function cookieJar(){
-    let cookies = 10; 
-
-
-    return function(){
-        if(cookies>0){
-            cookies--;
-            console.log("you eat one cooikie, cookies left: " + cookies);
-
-        }
-        else{
-            console.log("no cookies left");
-        }
-    }
-}
-
-let eatCookie = cookieJar();
-
-eatCookie();
-eatCookie();
-eatCookie();
-eatCookie();
-eatCookie();
-
-cookies--;
-eatCookie();
-console.log("cookies left: " + cookies); 
-
-
+// creates pvt variable -- anything inside stay hidden 
+// avoid pollution in global scope 
 
 
