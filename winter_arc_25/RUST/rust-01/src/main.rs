4698c885378adec4067e3580a6eb28ac0d47babf
mayank-vashishtt <mayankvashishtt@gmail.com>
2025-10-31 23:50:36 +0530

    rust intro

---
+++ winter_arc_25/RUST/rust-01/src/main.rs
@@ -0,0 +1,19 @@
+
+
+fn main() {
+    let x: i32 = 12;
+
+    println!("Hello, world!");
+    println!("{}",x);
+}
+
+// you can change the value of the variable in rust 
+// u have to tell it explicitly if u wanna do this shit .. crazy huh
+// let mut x: i8 = 12;
+
+//
+
+
+// 
+
+
---
+++ winter_arc_25/RUST/rust-01/target/debug/rust-01.d
@@ -0,0 +1,1 @@
+/Users/mayankvashisht/Desktop/classes/winter_arc_25/RUST/rust-01/target/debug/rust-01: /Users/mayankvashisht/Desktop/classes/winter_arc_25/RUST/rust-01/src/main.rs
